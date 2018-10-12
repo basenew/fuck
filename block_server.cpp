1b657f062fc24f469bb748a4f7fd9db2781e8852
basenew <190745247@qq.com>
2018-10-12 11:43:20 +0800

    rm tmp

---
+++ block_server.cpp
@@ -0,0 +1,17 @@
+#ifndef __BLOCK_SERVER__
+#define __BLOCK_SERVER__
+
+#include "server.h"
+
+namespace comm
+{
+class BlockServer:public Server
+{
+public:
+	virtual 
+private:
+	BlockServer();	
+};
+}
+
+#endif
--- server.h
+++ server.h
@@ -4,9 +4,73 @@
 #include "log.h"
 #include "connect.h"
 
+namespace comm
+{
 class Server
 {
 public:
+	enum SelectMode
+	{
+		NONE,
+		EPOLL,
+		SELECT,
+		POLL,
+	};
+
+	enum Proto
+	{
+		TCP,
+		UDP, 
+	};
+
+	enum Module
+	{
+		NONE = 0,
+		ECHO = 1,
+		P2P=2,
+		P2G = 4,
+		GROUP = 8,
+		BROCAST = 16,
+		STAT = 32,
+	};
+
+	struct NetConfig
+	{
+		Proto proto;	
+		bool  block;	
+		bool  delay;	
+		bool  keep_alive;
+		bool  echo;
+		int   max_conn;
+		int   max_preconn_que;
+		int   max_rcv_buf_size;
+		int   max_snd_buf_size;
+		int   heartbeat_timeout;
+	};
+	
+	struct MemConfig
+	{
+		
+	};
+
+	struct LogConfig
+	{
+		string path;
+		string name;
+	};
+
+	struct Config
+	{
+		ushort       port;
+		bool         demon;
+		int          proc_cnt;
+		SelectMode   mode;
+		uint32       modules,
+		NetConfig    net_conf,
+		MemConfig    mem_conf,
+		ThreadConfig thd_conf;
+	};
+
 	enum Err
 	{
 		ERR_INNER = -1,
@@ -14,7 +78,6 @@ public:
 	
 	using clientid int;
 public:
-	Server(uint16 port = 9898, int thread_count = 2, bool block = false);
 	virtual ~Server();
 	virtual void run();
 	virtual void read(clientid id, pcchar buf, int size);
@@ -22,10 +85,16 @@ public:
 	virtual void client_connected(client id);
 	virtual void client_close(client id);
 	virtual void shut_down();
-private:
-	short _port;
 	
+	static Server& ins()
+	{
+		static Server svr;
+		return svr;
+	};
+private:
+	Config _conf;	
+	Server();
 };
 
-
+}
 #endif
