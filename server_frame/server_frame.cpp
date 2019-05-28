c9e2a070c5eb1fb66463ec4162e7201a1ebfd0aa
yang.zou <yang.zou@ubtrobot.com>
2019-05-28 19:53:48 +0800

    修改代码

--- server_frame/server_frame.cpp
+++ server_frame/server_frame.cpp
@@ -1,50 +1,72 @@
 #include "server_frame.h"
+#include <chrono>
+
+using namespace std;
+using namespace std::chrono;
 
 namespace comm
 {
-
 	int server_frame::start(int argc, char** argv)
 	{
+		LOG_SYS << "server start..." << endl;
 		if (0 != parse_arg(argc, argv))
 			return -1;
 		
+		if (_status == STATUS_STOPED)
+			_thd = thread(thread_static_proc, this);
+	}
+
+	int server_frame::thread_proc()
+	{
 		LOG_SYS << "server start..." << endl;
 		on_start();
+		
+		_status = STATUS_RUNNING;
 		LOG_SYS << "server running..." << endl;
-		while (1)
+
+		std::chrono::milliseconds sleep_ms(10);
+		while (true)
 		{
-			_evt_egn.check(10);	
 			switch(_status)
 			{
-			case STATUS_PRESTOP:
+			case STATUS_STOPED:
+				goto THREAD_STOP;
 				break;
 			case STATUS_RELOAD:
 				break;
 			case STATUS_SUSPEND:
+				this_thread.sleep_for(sleep_ms);
 				break;
-			case STATUS_CONTINUE:
+			case STATUS_RUNNING:
+				_evt_egn.check(10);	
 				break;
 				
 			}
 			
 		}
 
+THREAD_STOP:
+
 		LOG_SYS << "server stop..." << endl;
 		
 		on_stop();
-		return 0;
+		_status = STATUS_STOPED;
+
+		return OK;
+
 	}
 
-	void server_frame::stop()
+	int server_frame::stop()
 	{
 		LOG_SYS << "call server to stop" << endl;
 		_status = STATUS_PRESTOP;
 	}
 
-	void server_frame::send_sig(int sig)
+	int server_frame::send_sig(int sig)
 	{
 	}
-	void server_frame::show_help()
+
+	int server_frame::show_help()
 	{
 	}
 	
--- server_frame/server_frame.h
+++ server_frame/server_frame.h
@@ -3,6 +3,9 @@
 #include "../comm/comm.h"
 #include "reactor.h"
 #include "io_handler.h"
+#include <thread>
+
+using namespace std;
 
 namespace comm
 {
@@ -16,34 +19,48 @@ public:
 		STATUS_STOPED,
 		STATUS_RUNNING,
 		STATUS_SUSPEND,
-		STATUS_CONTINUE,
-		STATUS_PRESTOP,
 		STATUS_RELOAD, 
 	};
 
-	server_frame(reactor& evt_egn, io_handler& main_io):_evt_egn(evt_egn), _main_io(main_io), _status(STATUS_STOPED){};
+	server_frame(reactor& evt_egn, io_handler& main_io)
+	:_evt_egn(evt_egn)
+	,_main_io(main_io)
+	,_status(STATUS_STOPED)
+	{};
+
 	virtual ~server_frame(){};
 	virtual int start(int argc, char** argv);
-	void stop();
+	virtual int stop();
+	virtual int pause();
+	virtual int resume();
+	virtual int reload();
 
-	virtual void send_sig(int sig);
-	virtual void on_start(){};
-	virtual void on_stop(){};
-	virtual void on_suspend(){};
-	virtual void on_continue(){};
-	virtual void on_reload(){};
+	virtual int send_sig(int sig);
+	virtual int on_start(){return OK;};
+	virtual int on_stop(){return OK;};
+	virtual int on_suspend(){return OK;};
+	virtual int on_continue(){return OK;};
+	virtual int on_reload(){return OK;};
 
 private:
-	server_frame(const server_frame&);
-	server_frame& operator=(const server_frame&);
+	server_frame(const server_frame&){};
+	server_frame& operator=(const server_frame&){};
+	static void thread_static_proc(server_frame* self);
+	{
+		if (self)
+			self->thread_proc();
+	};
+
+	int thread_proc();
 
 	void show_help();
 	int parse_arg(int argc, char** argv);
 
 private:
-	reactor& _evt_egn;
+	reactor&    _evt_egn;
 	io_handler& _main_io;
-	int _status;
+	thread      _thd;
+	int         _status;
 };
 
 }
